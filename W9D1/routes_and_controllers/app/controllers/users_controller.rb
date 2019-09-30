551d1d092df53647a04570d9bbf961ade74b1332
alex629lee <alex629lee@yahoo.com>
2019-09-30 16:44:20 -0700

    Added W9D1- First Routes and Controllers project

---
+++ W9D1/routes_and_controllers/app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+  def index
+    users = User.all
+    render json: users
+  end 
+
+  def create
+    user = User.new(params.require(:user).permit(:name, :email))
+    # replace the `user_attributes_here` with the actual attribute keys
+    if user.save
+        render json: user
+    else 
+        render json: user.errors.full_messages, status: :unprocessable_entity
+    end 
+  end 
+
+  def show
+    user = User.find(params[:id])
+    render json: user 
+  end
+end
